7d227f692dbed56ae0b22167ab0e2a6933ef3b05
Carlos Gómez <caragomezher@unal.edu.co>
2025-03-25 14:39:16 -0500

    Add more methods

--- src/controllers/document.js
+++ src/controllers/document.js
@@ -1,57 +1,82 @@
-import express from 'express';
-import formidable from 'formidable'; // 
-import ia from '../core/ia.js'
-import firebase from '../core/firebase.js'
+import express from "express";
+import formidable from "formidable"; //
+import ia from "../core/ia.js";
+import firebase from "../core/firebase.js";
 
 const router = express.Router();
+const COLLECTION_NAME = "cvs"
 
-router.post('/', async (req, res) => {
-    const prompt = req.body
-    if (prompt.prompt) {
-        const resp = await ia.getDocumentData(prompt.prompt)
-        res.status(200).json({ data: resp })
-    } else {
-        res.status(404).json({ message: "No prompt entered" })
-    }
-})
-
-router.post('/upload', async (req, res) => {
-    const form = formidable({ multiples: false }); // Configure formidable for single file upload
-
-    form.parse(req, async (err, fields, files) => {
-        if (err) {
-            console.error("Form parsing error:", err);
-            return res.status(500).json({ message: "Error parsing form data.", error: err });
-        }
-
-        // Check if file exists
-        if (!files.myFile) {
-            return res.status(400).json({ message: "No files were uploaded." });
-        }
-
-        const file = Array.isArray(files.myFile) ? files.myFile[0] : files.myFile; // Ensure compatibility for single uploads
-        const pdfFilePath = file.filepath;
-
-        try {
-            // Perform PDF analysis
-            const analysisResult = await ia.analyzePdf(pdfFilePath);
-
-            if (analysisResult) {
-                const uniqueId = `cv_${Date.now()}`; // Generate unique ID using timestamp
-                const added = await firebase.addData("cvs", uniqueId, analysisResult);
-
-                if (added) {
-                    return res.status(200).json({ message: "File uploaded and added to DB successfully.", id: uniqueId });
-                } else {
-                    return res.status(500).json({ message: "File saved, but data not added to DB." });
-                }
-            } else {
-                return res.status(500).json({ message: "File uploaded, but analysis failed." });
-            }
-        } catch (error) {
-            console.error("Error during analysis:", error);
-            return res.status(500).json({ message: "File uploaded, but analysis encountered an error.", error: error });
-        }
-    });
+router.post("/", async (req, res) => {
+  const prompt = req.body;
+  if (prompt.prompt) {
+    const resp = await ia.getDocumentData(prompt.prompt);
+    res.status(200).json({ data: resp });
+  } else {
+    res.status(404).json({ message: "No prompt entered" });
+  }
 });
+
+router.post("/upload", async (req, res) => {
+  const form = formidable({ multiples: false }); // Configure formidable for single file upload
+
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      console.error("Form parsing error:", err);
+      return res
+        .status(500)
+        .json({ message: "Error parsing form data.", error: err });
+    }
+
+    // Check if file exists
+    if (!files.myFile) {
+      return res.status(400).json({ message: "No files were uploaded." });
+    }
+
+    const file = Array.isArray(files.myFile) ? files.myFile[0] : files.myFile; // Ensure compatibility for single uploads
+    const pdfFilePath = file.filepath;
+
+    try {
+      // Perform PDF analysis
+      const analysisResult = await ia.analyzePdf(pdfFilePath);
+
+      if (analysisResult) {
+        const uniqueId = `cv_${Date.now()}`; // Generate unique ID using timestamp
+        const added = await firebase.addData(COLLECTION_NAME, uniqueId, analysisResult);
+
+        if (added) {
+          return res
+            .status(200)
+            .json({
+              message: "File uploaded and added to DB successfully.",
+              id: uniqueId,
+            });
+        } else {
+          return res
+            .status(500)
+            .json({ message: "File saved, but data not added to DB." });
+        }
+      } else {
+        return res
+          .status(500)
+          .json({ message: "File uploaded, but analysis failed." });
+      }
+    } catch (error) {
+      console.error("Error during analysis:", error);
+      return res
+        .status(500)
+        .json({
+          message: "File uploaded, but analysis encountered an error.",
+          error: error,
+        });
+    }
+  });
+});
+
+router.get("/cvs", async (req, res) => {
+  const results = await firebase.getAllData(COLLECTION_NAME);
+  return results
+    ? res.status(200).json(results)
+    : res.status(500).json({ message: "No results recovered" });
+});
+
 export default router;
--- src/core/firebase.js
+++ src/core/firebase.js
@@ -1,20 +1,20 @@
-import { initializeApp, cert } from 'firebase-admin/app';
-import { getFirestore, Timestamp } from 'firebase-admin/firestore';
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-import serviceAccount from '../../secrets/serviceAccountKey.json' assert { type: "json"}
+import serviceAccount from "../../secrets/serviceAccountKey.json" with { type: "json" }
 
 initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
 const db = getFirestore();
 
 async function addData(collectionName, documentId, data) {
   try {
     await db.collection(collectionName).doc(documentId).set(data);
-    console.log('Document written with ID: ', documentId);
+    console.log("Document written with ID: ", documentId);
     return true;
   } catch (error) {
-    console.error('Error adding document: ', error);
+    console.error("Error adding document: ", error);
     return false;
   }
 }
@@ -23,53 +23,53 @@ async function getData(collectionName, documentId) {
   try {
     const doc = await db.collection(collectionName).doc(documentId).get();
     if (doc.exists) {
-      console.log('Document data:', doc.data());
+      console.log("Document data:", doc.data());
       return doc.data();
     } else {
-      console.log('No such document!');
+      console.log("No such document!");
       return null;
     }
   } catch (error) {
-    console.error('Error getting document: ', error);
+    console.error("Error getting document: ", error);
     return null;
   }
 }
 
 async function updateData(collectionName, documentId, data) {
-    try{
-        await db.collection(collectionName).doc(documentId).update(data);
-        console.log("Document successfully updated!");
-        return true;
-    } catch (error){
-        console.error("Error updating document: ", error);
-        return false;
-    }
+  try {
+    await db.collection(collectionName).doc(documentId).update(data);
+    console.log("Document successfully updated!");
+    return true;
+  } catch (error) {
+    console.error("Error updating document: ", error);
+    return false;
+  }
 }
 
-async function deleteData(collectionName, documentId){
-    try{
-        await db.collection(collectionName).doc(documentId).delete();
-        console.log("Document successfully deleted!");
-        return true;
-    } catch(error){
-        console.error("Error deleting document:", error);
-        return false;
-    }
+async function deleteData(collectionName, documentId) {
+  try {
+    await db.collection(collectionName).doc(documentId).delete();
+    console.log("Document successfully deleted!");
+    return true;
+  } catch (error) {
+    console.error("Error deleting document:", error);
+    return false;
+  }
 }
 
-async function getAllData(collectionName){
-    try{
-        const snapshot = await db.collection(collectionName).get();
-        const results = [];
-        snapshot.forEach(doc => {
-            results.push({id: doc.id, data: doc.data()});
-        });
-        console.log("All data from collection:", results);
-        return results;
-    } catch(error){
-        console.error("Error getting all data:", error);
-        return null;
-    }
+async function getAllData(collectionName) {
+  try {
+    const snapshot = await db.collection(collectionName).get();
+    const results = [];
+    snapshot.forEach((doc) => {
+      results.push({ id: doc.id, data: doc.data() });
+    });
+    console.log("All data from collection:", results);
+    return results;
+  } catch (error) {
+    console.error("Error getting all data:", error);
+    return null;
+  }
 }
 
-export default {addData, getData, updateData, deleteData, getAllData};
+export default { addData, getData, updateData, deleteData, getAllData };
