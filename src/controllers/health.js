6b14bcd452b73c68d98d7457cd2eea72455a0184
Carlos Gómez <caragomezher@unal.edu.co>
2025-03-27 16:16:49 +0000

    Fix grammar

--- src/controllers/health.js
+++ src/controllers/health.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (res, req) => {
-    res.status(200).json({ message: "Healthly" })
+    res.status(200).json({ message: "Healthy" })
 })
 
 export default router;
